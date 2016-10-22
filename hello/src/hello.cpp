11f8faa42968c61207804b64b300b4f44e4b3e7d
alvaromorte <alvaromorte@gmail.com>
2016-10-22 16:22:36 +0200

    mas hello
    incorpora hello

---
+++ hello/src/hello.cpp
@@ -0,0 +1,17 @@
+//============================================================================
+// Name        : hello.cpp
+// Author      : AMM
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <string.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+	string nombre, nombre1;
+	cout << "!!! Hello World!!ccccccccccccccccccccccccf" << endl; // prints !!! Hello World !!!
+	return 0;
+}
